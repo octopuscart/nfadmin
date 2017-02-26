c537cade5e12dfc803b5ade43ef23ea881e00699
ranirathore <rani21rathore@gmail.com>
2017-02-26 21:13:52 +0530

    changes

--- application/views/userRecordManagement/userProfileRecord.php
+++ application/views/userRecordManagement/userProfileRecord.php
@@ -22,9 +22,7 @@ $data['userProfile'] = $this->Product_model->get_table_information('auth_user');
                         <thead>
                             <tr>
                                 <th style="width: 7%;">S.No.</th>
-                                <th>Client Code</th>
-                                <th>Client Name</th>
-                                <th>Email/Contact No.</th>
+                                <th>Client Details</th>
                                 <th style="width: 120px">Birth Date</th>
                                 <th style="width: 120px">Joining Date</th>
                                 <th style="width:300px;">View Profile</th> 
@@ -39,23 +37,31 @@ $data['userProfile'] = $this->Product_model->get_table_information('auth_user');
                                 //print_r($userProfile);
                                 foreach ($userProfile as $key => $value) {
                                     $table_name = "auth_user";
-                           
                                     ?>
-                                    
+
                                     <tr>
                                         <td><?php echo $count ?> </td>
-                                        <td class="capitalize"><?php echo $value['registration_id'] ?> </td>
-                                        <td class="capitalize"><?php echo $value['first_name'] . ' ' . $value['middle_name'] . ' ' . $value['last_name'] ?> </td>
-                                        <td><?php echo $value['email'] ?> 
-                                            <br/>
-                                            <?php echo $value['contact_no'] ?> </td>
+                                        <td>
+                                            <p class="capitalize" style="margin: 0 0 0px;">
+                                                <b> <?php echo $value['registration_id'] ?></b> 
+                                            </p>
+                                            <p class="capitalize"  style="margin: 0 0 0px;">
+                                                <i class="fa fa-user"></i> <?php echo $value['first_name'] . ' ' . $value['middle_name'] . ' ' . $value['last_name'] ?> 
+                                            </p>
+                                            <p style="margin: 0 0 0px;">
+                                                <i class="fa fa-envelope"></i> <?php echo $value['email'] ?> 
+                                            </p>
+                                            <p  style="margin: 0 0 0px;">
+                                                <i class="fa fa-phone"></i> <?php echo $value['contact_no'] ?>
+                                            </p>
+                                        </td>
                                         <td><?php echo $value['birth_date'] ?>
                                         </td>
                                         <td>
                                             <?php echo $value['joining_date'] ?> </td>
                                         <td >
 
-                                <!--                                      <a class="btn btn-success btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_profile_view_info/<?php echo $value['id'] ?>"> View</a>-->
+                                                                <!--                                      <a class="btn btn-success btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_profile_view_info/<?php echo $value['id'] ?>"> View</a>-->
                                             <a style="margin-left:4px;" class="btn btn-info btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_detail_info/<?php echo $value['id'] ?>"> Detail</a>
                                             <a class="btn btn-danger btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_login_record/<?php echo $value['id'] ?>"> Login Record</a>
                                             <a href="<?php echo base_url() ?>../frontend/views/frontend_login_from_adminpanel.php?email=<?php echo $value['email']; ?>&password=<?php echo $value['password']; ?>&table_name=<?php echo $table_name; ?>&userId=<?php echo $value['id'] ?>" class="btn btn-warning btn-xs m-r-5" target="_blank">Login as User</a>
@@ -93,11 +99,9 @@ $data['userProfile'] = $this->Product_model->get_table_information('auth_user');
                         <thead>
                             <tr>
                                 <th style="width: 7%;">S.No.</th>
-                                <th>Client Code</th>
-                                <th>Client Name</th>
-                                <th>Email/Contact No.</th>
+                                 <th>Client Details</th>
                                 <th style="width: 120px">Birth Date</th>
-                                 <th style="width: 120px">Joining Date</th>
+                                <th style="width: 120px">Joining Date</th>
                                 <th style="width:280px;">View Profile</th> 
                             </tr>
                         </thead>
@@ -114,19 +118,28 @@ $data['userProfile'] = $this->Product_model->get_table_information('auth_user');
 
                                     <tr>
                                         <td><?php echo $count ?> </td>
-                                        <td class="capitalize"><?php echo $value['registration_id'] ?> </td>
-                                        <td class="capitalize"><?php echo $value['first_name'] . ' ' . $value['middle_name'] . ' ' . $value['last_name'] ?> </td>
-                                        <td><?php echo $value['email'] ?> 
-                                            <br/>
-                                            <?php echo $value['contact_no'] ?> </td>
-                                        <td><?php echo $value['birth_date'] ?>
+                                        <td>
+                                            <p class="capitalize" style="margin: 0 0 0px;">
+                                                <b> <?php echo $value['registration_id'] ?></b> 
+                                            </p>
+                                            <p class="capitalize"  style="margin: 0 0 0px;">
+                                                <i class="fa fa-user"></i> <?php echo $value['first_name'] . ' ' . $value['middle_name'] . ' ' . $value['last_name'] ?> 
+                                            </p>
+                                            <p style="margin: 0 0 0px;">
+                                                <i class="fa fa-envelope"></i> <?php echo $value['email'] ?> 
+                                            </p>
+                                            <p  style="margin: 0 0 0px;">
+                                                <i class="fa fa-phone"></i> <?php echo $value['contact_no'] ?>
+                                            </p>
+                                        </td>
+                                         <td><?php echo $value['birth_date'] ?>
                                         </td>
                                         <td>
                                             <?php echo $value['joining_date'] ?> </td>
-                                      
+
                                         <td>
 
-                                <!--                                      <a class="btn btn-success btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_profile_view_info/<?php echo $value['id'] ?>"> View</a>-->
+                                                                <!--                                      <a class="btn btn-success btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_profile_view_info/<?php echo $value['id'] ?>"> View</a>-->
                                             <a style="margin-left:4px;" class="btn btn-info btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_detail_info/<?php echo $value['id'] ?>"> Detail</a>
                                             <a class="btn btn-danger btn-xs m-r-5" href="<?php echo base_url() ?>index.php/UserRecordManagement/user_login_record/<?php echo $value['id'] ?>"> Login Record</a>
                                             <a href="<?php echo base_url() ?>../frontend/views/frontend_login_from_adminpanel.php?email=<?php echo $value['email']; ?>&password=<?php echo $value['password']; ?>&table_name=<?php echo $table_name; ?>&userId=<?php echo $value['id'] ?>" class="btn btn-warning btn-xs m-r-5" target="_blank">Login as User</a>
@@ -157,7 +170,7 @@ $this->load->view('layout/layoutBottom');
         App.init();
 
         TableManageDefault.init();
-    
+
 //         FormPlugins.init();
         $("#data_table1").DataTable(
                 {"pageLength": 25});
